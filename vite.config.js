12082c8dd5f1b0900a19466d01e3026a87d34639
Maksim Leskin <Maksim.leskin@methed.ru>
2023-10-22 18:30:09 +0300

    add Legacy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import autoprefixer from "autoprefixer";
-// import Legacy from "@vitejs/plugin-legacy";
+import Legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
   plugins: [
